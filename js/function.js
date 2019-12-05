f6d50cd20ff4a2f0d7c001d3725cd9423dda03e9
Limlift <ironstrawman@gmail.com>
2019-12-05 16:51:27 +0800

    Site updated: 2019-12-05 16:51:27

--- js/function.js
+++ js/function.js
@@ -11,4 +11,19 @@ $(function() {
 	var char = window.location.href;
 	char = char.toString();
 	checkMenu(char);
+	$(document).scroll(function() {
+		var top = $(document).scrollTop();
+		console.log(top);
+		if (0<top&&top<160) {
+			$('header .large').css('height',160-top/160*80);
+			$('header').css('background-color','rgba(255,255,255,1)');
+			$('header').css('transform','translated3d(0px,0px,0px)');
+		}else if(top>=160){
+			$('header .large').css('height','80px');
+			$('header').css('transform','translated3d(0px,-180px,0px)');
+		}else{
+			$('header').css('background-color','rgba(255,255,255,.8)');
+		}
+	})
+	
 })
